14975eddffa90b57450f8e0ef75e1cf217a4423b
air2ria <semitair4@gmail.com>
2026-03-13 22:55:41 -0400

    Update EulerRotation.cpp

--- EulerRotation.cpp
+++ EulerRotation.cpp
@@ -1,3 +1,5 @@
+#include "EulerRotation.hpp"
+
 bool EulerRotation::operator==(const EulerRotation& other) const {
     return std::abs(roll - other.roll) < 0.001 &&
            std::abs(pitch - other.pitch) < 0.001 &&
