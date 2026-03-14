26d18306ba872b31144e4939cda87925cbd4ca89
air2ria <semitair4@gmail.com>
2026-03-13 22:55:57 -0400

    Update AxisAngleRotation.cpp

--- AxisAngleRotation.cpp
+++ AxisAngleRotation.cpp
@@ -1,3 +1,5 @@
+#include "AxisAngleRotation.hpp"
+
 bool AxisAngleRotation::operator==(const AxisAngleRotation& other) const {
     return axis == other.axis &&
            std::abs(angle - other.angle) < 0.001;
